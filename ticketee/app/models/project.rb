a8d99ff682f4715390d5f7118c64d19193bc3aa0
James Skemp <strivinglife@gmail.com>
2014-08-23 18:21:02 -0500

    Delete tickets associated with a project when they are deleted.

--- ticketee/app/models/project.rb
+++ ticketee/app/models/project.rb
@@ -1,5 +1,9 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
 
-  has_many :tickets
+  # valid dependencies:
+  # :destroy (use callbacks)
+  # :delete_all (DELETE FROM tickets WHERE project_id = :project_id)
+  # :nullify (UPDATE tickets SET project_id = null WHERE project_id = :project_id)
+  has_many :tickets, dependent: :delete_all
 end
