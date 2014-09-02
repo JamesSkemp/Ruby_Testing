5dad4791a3266ab200f881d12de9b0fe18ee4718
James Skemp <strivinglife@gmail.com>
2014-09-02 18:11:02 -0500

    Implemented functionality for users to view their profile.

--- ticketee/spec/features/signing_up_spec.rb
+++ ticketee/spec/features/signing_up_spec.rb
@@ -8,7 +8,7 @@ feature "Signing up" do
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    click_button "Sign Up"
 
     expect(page).to have_content("You have signed up successfully.")
   end
